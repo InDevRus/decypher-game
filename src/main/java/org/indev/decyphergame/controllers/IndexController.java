886124c3541554af1dfdd22e598a3fd8202a5b97
InDev <InDevRus@hotmail.com>
2019-10-30 21:16:35 +0500

    Добавил базовый контроллер со страницей

---
+++ src/main/java/org/indev/decyphergame/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package org.indev.decyphergame.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String greeting(@RequestParam(name = "playerName", required = false, defaultValue = "Player") String name, Model model) {
+        model.addAttribute("playerName", name);
+        return "index";
+    }
+}
