b7207310f05e492a880480337de9fb7334234dee
InDev <InDevRus@hotmail.com>
2019-10-30 21:12:34 +0500

    Создал каркас приложения

---
+++ src/main/java/org/indev/decyphergame/DecypherGameApplication.java
@@ -0,0 +1,11 @@
+package org.indev.decyphergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecypherGameApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DecypherGameApplication.class, args);
+	}
+}
